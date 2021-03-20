e8a26079e663822fa6476df4e7b0c09a6c002ff4
Konstantin Münster <konstantin.muenster@mail.com>
2021-03-20 13:46:03 +0100

    Fix: Use husky@4

--- gatsby-theme-portfolio-minimal/.eslintrc.js
+++ gatsby-theme-portfolio-minimal/.eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        ecmaVersion: 2018,
+        sourceType: 'module',
         ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
+            jsx: true,
         },
     },
 };
