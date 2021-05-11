66371a3a4e4194b6e3d1acbe388edd5baacad477
Konstantin <konstantin.muenster@mail.com>
2021-05-11 17:23:40 +0200

    style: add favicon to example site

--- example-site/gatsby-config.js
+++ example-site/gatsby-config.js
@@ -2,23 +2,23 @@ module.exports = {
     plugins: [
         {
             resolve: 'gatsby-theme-portfolio-minimal',
-            // options: {
-            //     favicon: "", // e.g. "./content/favicon.png"
-            //     siteUrl: "https://example.com", // Used for sitemap generation
-            //     manifestSettings: {
-            //         siteName: "My Minimal Portfolio", // Used in manifest.json
-            //         shortName: "Portfolio", // Used in manifest.json
-            //         startUrl: "/", // Used in manifest.json
-            //         backgroundColor: "#FFFFFF", // Used in manifest.json
-            //         themeColor: "#000000", // Used in manifest.json
-            //         display: "minimal-ui", // Used in manifest.json
-            //     },
-            //     googleAnalytics: {
-            //         trackingId: "UA-XXXXXX-X",
-            //         anonymize: true, // Default true
-            //         environments: ["production", "development"] // Default ["production"]
-            //     }
-            // },
+            options: {
+                // siteUrl: "https://example.com", // Used for sitemap generation
+                manifestSettings: {
+                    favicon: './content/images/favicon.png', // Path is relative to the root
+                    siteName: 'My Minimal Portfolio', // Used in manifest.json
+                    shortName: 'Portfolio', // Used in manifest.json
+                    startUrl: '/', // Used in manifest.json
+                    backgroundColor: '#FFFFFF', // Used in manifest.json
+                    themeColor: '#000000', // Used in manifest.json
+                    display: 'minimal-ui', // Used in manifest.json
+                },
+                // googleAnalytics: {
+                //     trackingId: "UA-XXXXXX-X",
+                //     anonymize: true, // Default true
+                //     environments: ["production", "development"] // Default ["production"]
+                // }
+            },
         },
         {
             resolve: `gatsby-source-filesystem`,
