25b35d51f4366ed3cd108b8f871810d277116c19
Konstantin <konstantin.muenster@mail.com>
2021-05-13 17:10:34 +0200

    fix: remove SplashScreen from exported components
    SplashScreen component can't be exported from the package

--- gatsby-theme-portfolio-minimal/index.js
+++ gatsby-theme-portfolio-minimal/index.js
@@ -1,6 +1,5 @@
 export { Page } from './src/components/Page';
 export { Seo } from './src/components/Seo';
-export { SplashScreen } from './src/components/SplashScreen';
 export { HeroSection } from './src/sections/Hero';
 export { ArticlesSection } from './src/sections/Articles';
 export { AboutSection } from './src/sections/About';
