b193ba2d9e79e26a674149fc374162d17e0a2c23
Konstantin <konstantin.muenster@mail.com>
2021-06-12 11:29:09 +0200

    fix: Add explicit types to avoid errors during setup
    By adding explicit types to the GraphQL schema, we do not need to add any dummy content during setup in order to see a first preview of the site

--- gatsby-theme-portfolio-minimal/src/gatsby/node/createSchemaCustomization.js
+++ gatsby-theme-portfolio-minimal/src/gatsby/node/createSchemaCustomization.js
@@ -5,6 +5,91 @@ module.exports = ({ actions }) => {
         alt: String
         caption: String
     }
+    type Image {
+        src: File @fileByRelativePath
+        alt: String
+    }
+    type SocialProfiles {
+        from: [String!]!
+        showIcons: Boolean
+    }
+    type HeroSubtitle {
+        prefix: String
+        highlight: String
+        suffix: String
+    }
+    type Interest {
+        label: String
+        image: Image
+    }
+    type InterestsButton {
+        visible: Boolean
+        label: String
+        initiallyShownInterests: Int
+    }
+    type ProjectLink {
+        type: String
+        url: String
+    }
+    type Project {
+        visible: Boolean
+        category: String
+        title: String
+        description: String
+        tags: [String]
+        image: Image
+        links: [ProjectLink]
+    }
+    type ProjectButton {
+        visible: Boolean
+        label: String
+        url: String
+    }
+    type Social {
+        behance: String
+        github: String
+        medium: String
+        linkedin: String
+        mail: String
+    }
+    type Logo {
+        text: String
+    }
+    type NavigationItem {
+        label: String
+        url: String
+    }
+    type CallToActionButton {
+        label: String
+        url: String
+        openNewTab: Boolean
+    }
+    type Navigation {
+        header: [NavigationItem]
+        footer: [NavigationItem]
+        ctaButton: CallToActionButton
+    }
+    type FeatureToggles {
+        useDarkModeAsDefault: Boolean
+        useDarkModeBasedOnUsersPreference: Boolean
+        useCookieBar: Boolean
+    }
+    type SiteMetadata {
+        language: String
+        siteUrl: String
+        title: String
+        titleTemplate: String
+        description: String
+        author: String
+        avatar: File @fileByRelativePath
+        bio: String
+        social: Social
+    }
+    type SiteConfiguration {
+        logo: Logo
+        navigation: Navigation
+        featureToggles: FeatureToggles
+    }
     interface Article implements Node {
         id: ID!
         slug: String!
@@ -16,7 +101,7 @@ module.exports = ({ actions }) => {
         categories: [String!]!
         keywords: [String!]
     }
-    type ArticleMarkdown implements Node & Article @childOf(type: "MarkdownRemark") {
+    type ArticleMarkdown implements Node & Article @dontInfer @childOf(type: "MarkdownRemark") {
         id: ID!
         slug: String!
         title: String!
@@ -26,6 +111,40 @@ module.exports = ({ actions }) => {
         banner: BannerImage
         categories: [String!]!
         keywords: [String!]
+    }
+    type MarkdownRemarkFrontmatter {
+        imageSrc: File @fileByRelativePath
+        imageAlt: String
+    }
+    type MarkdownRemark implements Node {
+        frontmatter: MarkdownRemarkFrontmatter
+    }
+    type ContactJson implements Node @dontInfer {
+        name: String
+        email: String
+        description: String
+        image: Image
+        socialProfiles: SocialProfiles
+    }
+    type HeroJson implements Node @dontInfer {
+        intro: String
+        image: Image
+        title: String
+        subtitle: HeroSubtitle
+        description: String
+        socialProfiles: SocialProfiles
+    }
+    type InterestsJson implements Node @dontInfer {
+        interests: [Interest]
+        button: InterestsButton
+    }
+    type ProjectsJson implements Node @dontInfer {
+        projects: [Project]
+        button: ProjectButton
+    }
+    type ContentJson implements Node @dontInfer {
+        siteMetadata: SiteMetadata
+        siteConfiguration: SiteConfiguration
     }
   `);
 };
