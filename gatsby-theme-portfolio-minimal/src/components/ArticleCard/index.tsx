3d614670c80812a5f90e95dc2a48b1bf8339da68
Konstantin <konstantin.muenster@mail.com>
2021-06-22 17:10:43 +0200

    refactor: Change date formatting on ArticleCard component

--- gatsby-theme-portfolio-minimal/src/components/ArticleCard/index.tsx
+++ gatsby-theme-portfolio-minimal/src/components/ArticleCard/index.tsx
@@ -96,5 +96,5 @@ export function ArticleCardSkeleton(): React.ReactElement {
 
 function formatDate(date: Date): string {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
